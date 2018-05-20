aefdcfd082238285cd916c81cb8bbe2d8d4fb124
mthaiseer <mohamed.thaiseer@gmail.com>
2018-05-20 20:07:06 +0400

    adding code for reactive form

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
+
+@Component({
+  selector: 'signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent {
+
+  form =  new FormGroup({
+    username : new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpaces
+    ]),
+    password : new FormControl('', Validators.required)
+  })
+
+  get username(){
+   return  this.form.get('username');
+  }
+}
